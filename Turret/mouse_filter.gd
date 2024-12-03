d21044ea67a5c65ac9d3e84bfd8b2d37d2a3fa43
evian-christ <58662680+evian-christ@users.noreply.github.com>
2024-12-03 10:37:18 +0000

    mouse entered trigger implemented

--- Turret/mouse_filter.gd
+++ Turret/mouse_filter.gd
@@ -1,11 +1,7 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.input_pickable = true
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.s
 func _process(delta: float) -> void:
 	pass
--- turret.gd
+++ turret.gd
@@ -1,16 +1,9 @@
 extends Node2D
 
-@onready var mouse_filter = $mouse_filter
-
-var attack_range
-var attack_range_visible
+@onready var mouse_detector = $mouse_detector
 
 func _ready() -> void:
-	attack_range_visible = false
-	attack_range = 100
-
-	# Establishing mask area connection
-	mouse_filter.connect("mouse_entered", Callable(self, "_on_mouse_entered"))
-
-func _on_mouse_entered():
-	print("entered")
+	mouse_detector.mouse_entered.connect(_on_mouse_detector_mouse_entered)
+	
+func _on_mouse_detector_mouse_entered() -> void:
+	print("")
